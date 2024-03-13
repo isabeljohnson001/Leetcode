7b79dac421ae9a8c2180e1b06cf3b8366d884266
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-03-13 14:05:33 -0400

    Time: 1 ms (81.20%) | Memory: 41.2 MB (89.47%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        String word1=prepareString(s);
+        String word2=prepareString(t);
+        if(word1.equals(word2))
+            return true;
+        else 
+            return false;
+        
+    }
+    
+    public String prepareString(String word){
+        StringBuilder s=new StringBuilder();
+        for(char c :word.toCharArray()){
+            if(c!='#'){
+                s.append(c);
+            }else{
+                if(s.length()>0)
+                    s.deleteCharAt(s.length()-1);
+            }
+        }
+        return s.toString();
+    }
+}
