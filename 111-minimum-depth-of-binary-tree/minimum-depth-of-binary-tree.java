97a58f9342686790e2a8a3aeb02a5a01d21190df
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-03-28 03:23:21 -0400

    Time: 6 ms (29.14%) | Memory: 63.4 MB (34.45%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -1,38 +1,19 @@
-class Pair{
-    TreeNode node;
-    int depth;
-    Pair(TreeNode node,int depth){
-        this.node=node;
-        this.depth=depth;
-    }
-}
-
 class Solution {
 
     public int minDepth(TreeNode root) {
 
-        if(root ==null){
+        return dfs(root);       
+    }
+
+    private int dfs(TreeNode root){
+        if(root==null){
             return 0;
         }
-        Stack<Pair> stack=new Stack();
-        stack.push(new Pair(root,1));
-        int ans=Integer.MAX_VALUE;
-
-        while(!stack.empty()){
-            Pair pair=stack.pop();
-            TreeNode node=pair.node;
-            int depth=pair.depth;
-            if(node.left==null && node.right==null){
-                ans=Math.min(ans,depth);
-            }
-            
-            if(node.left!=null){
-                stack.push(new Pair(node.left,depth+1));
-            }
-            if(node.right!=null){
-                stack.push(new Pair(node.right,depth+1));
-            }
+        if(root.left==null){
+            return 1+dfs(root.right);
+        }else if(root.right==null){
+            return 1+dfs(root.left);
         }
-        return ans;       
+        return 1+Math.min(dfs(root.left),dfs(root.right));
     }
 }
