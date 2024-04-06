7e643835d08582561bfe48be41a57b10fd5c20c1
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-04-06 16:09:14 -0400

    Time: 1 ms (68.48%) | Memory: 42.2 MB (15.49%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static List<Integer> rightSideView(TreeNode root) {
+            if (root == null) {
+                return new ArrayList<>();
+            }
+            int value=0;
+            List<TreeNode> que = new ArrayList<>();
+            List<Integer> ans = new ArrayList<>();
+            que.add(root);
+            while (!que.isEmpty()) {
+                int currentLevel = que.size();
+                for (int i = 0; i < currentLevel; i++) {
+                    TreeNode node = que.removeFirst();
+                    value = node.val;
+                    if(node.left!=null){que.add(node.left);}
+                    if(node.right!=null){que.add(node.right);}
+                }
+                ans.add(value);
+            }
+            return ans;
+
+        }
+}
