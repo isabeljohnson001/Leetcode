fa7955ded92fe516593fc0c6278ddae7916d7297
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-05-04 14:33:33 -0400

    Time: 80 ms (31.53%) | Memory: 18 MB (7.60%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -5,12 +5,11 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        hs = collections.defaultdict(list)
+        hs=collections.defaultdict(list)
         for s in strs:
-            count = [0]*26
+            counter=[0]*26
             for c in s:
-                count[ord(c) - ord('a')] += 1 
-            hs[tuple(count)].append(s)  
-
-        return list(map(list, hs.values()))
+                counter[ord(c)-ord("a")]+=1
+            hs[tuple(counter)].append(s)
+        return hs.values()
 
