761f2b621e00e2bcbd4461bc0c3fe9988955f7f8
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-05-06 16:00:18 -0400

    Time: 179 ms (59.72%) | Memory: 12.9 MB (60.37%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        mid=0
+        left=0
+        right=len(nums)-1
+        while(left<=right):
+            mid=(left+right)/2
+            num=nums[mid]
+            if(num==target):
+                return mid
+            if target<num:
+                right=mid-1
+            if target>num:
+                left=mid+1
+        return -1
