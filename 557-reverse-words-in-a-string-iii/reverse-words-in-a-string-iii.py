56689cd97a8623a8a8b5862acbe310a288f6cd4a
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-09-03 14:45:32 -0400

    Time: 52 ms (22.62%) | Memory: 12.5 MB (96.49%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -5,10 +5,13 @@ class Solution(object):
         :rtype: str
         """
         arr=s.split(' ')
-        left=0
-        right=0
         for i in range(len(arr)):
-            curr_word=arr[i]
-            arr[i] = curr_word[::-1] 
+            curr_word=list(arr[i])
+            left,right=0,len(curr_word)-1
+            while left<right:
+                curr_word[left],curr_word[right]=curr_word[right],curr_word[left]
+                left+=1
+                right-=1
+            arr[i]=''.join(curr_word)
         return ' '.join(arr)
-        
+            
