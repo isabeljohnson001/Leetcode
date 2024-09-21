c3d79affe664c04bfb0086220d47767487ca3fae
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-09-21 13:43:54 -0400

    Time: 509 ms (29.19%) | Memory: 18.1 MB (52.69%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,36 +1,43 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-
-        max_row=len(grid)-1
-        max_col=len(grid[0])-1
-        directions=[(-1,-1),(-1,0),(0,-1),(0,1),(1,0),(1,1),(1,-1),(-1,1)]
-
-
-        def get_neighbours(row,col):
-            for row_diff,col_diff in directions:
-                new_row=row+row_diff
-                new_col=col+col_diff
-                if not (0 <= new_row <= max_row and 0 <= new_col <= max_col):
+        
+        max_row = len(grid) - 1
+        max_col = len(grid[0]) - 1
+        directions = [
+            (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        
+        # Helper function to find the neighbors of a given cell.
+        def get_neighbours(row, col):
+            for row_difference, col_difference in directions:
+                new_row = row + row_difference
+                new_col = col + col_difference
+                if not(0 <= new_row <= max_row and 0 <= new_col <= max_col):
                     continue
-                if grid[new_row][new_col]!=0:
+                if grid[new_row][new_col] != 0:
                     continue
-                yield(new_row,new_col)
-
-        if grid[0][0]!=0 or grid[max_row][max_col]!=0:
+                yield (new_row, new_col)
+        
+        # Check that the first and last cells are open. 
+        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
             return -1
         
-        queue=deque()
-        queue.append((0,0))
-        grid[0][0]=1
-
+        # Set up the BFS.
+        queue = deque([(0, 0, 1)])
+        visited = {(0, 0)}
+        
+        # Do the BFS.
         while queue:
-            row,col=queue.popleft()
-            distance=grid[row][col]
-            if (row,col)==(max_row,max_col):
+            row, col, distance = queue.popleft()
+            if (row, col) == (max_row, max_col):
                 return distance
-            for neighbour_row,neighbor_col in get_neighbours(row,col):
-                grid[neighbour_row][neighbor_col]=distance+1
-                queue.append((neighbour_row,neighbor_col))
+            for neighbour in get_neighbours(row, col):
+                if neighbour in visited:
+                    continue
+                visited.add(neighbour)
+                # Note that the * splits neighbour into its values.
+                queue.append((*neighbour, distance + 1))
+                
+        # There was no path.
         return -1
 
 
