983ffddbd44c11303ab6095781205166ecd0c78b
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-10-06 23:09:04 -0400

    Time: 236 ms (17.91%) | Memory: 23.3 MB (74.47%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+
+
+        queue=collections.deque()
+        queue.append(start)
+        n=len(arr)
+        while queue:
+
+            node=queue.popleft()
+            if arr[node]==0:
+                return True
+            if arr[node]<0:
+                continue
+
+            for i in [node-arr[node],node+arr[node]]:
+                if 0<=i<n:
+                    queue.append(i)
+            arr[node]=-arr[node]
+        
+        return False
