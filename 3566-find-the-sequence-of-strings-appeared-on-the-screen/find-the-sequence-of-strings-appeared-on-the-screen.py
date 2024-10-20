eb0ed0357ffdd92ac8ef158eb53dc03d4e6147c1
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-10-20 15:24:06 -0400

    Time: 84 ms (100.00%) | Memory: 25.8 MB (100.00%) - LeetSync

---
+++ 3566-find-the-sequence-of-strings-appeared-on-the-screen/find-the-sequence-of-strings-appeared-on-the-screen.py
@@ -0,0 +1,17 @@
+class Solution:
+    def stringSequence(self, target: str) -> List[str]:
+
+        n=len(target)
+
+        result=[]
+        current_str=[""]*n
+
+        for i in range(n):
+
+            current_str[i]="a"
+            for ch in range(ord("a"),ord(target[i])+1):
+                current_str[i]=chr(ch)
+
+                result.append("".join(current_str))
+
+        return result
