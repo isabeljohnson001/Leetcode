1a9bf97c61ea411992dc215342f15848dbe7f7e5
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-11-06 16:27:18 -0500

    Time: 107 ms (65.58%) | Memory: 33.3 MB (42.89%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def getAverages(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if(k==0):
+            return nums
+        n=len(nums);
+        windowSize=2*k+1;
+        #averages=[]
+        #for i in range(0,n):
+        #    averages.append(-1)
+        averages=[-1]*n
+        prefix=[]
+        if(windowSize>n):
+            return averages
+        prefix=[0] * (n + 1)
+        for i in range(0,n):
+            prefix[i+1]=prefix[i]+nums[i]
+        for i in range(k,n-k):
+            leftBound=i-k
+            rightBound=i+k
+            sum=prefix[rightBound+1]-prefix[leftBound]
+            average=sum/windowSize
+            averages[i]=average
+        return averages
