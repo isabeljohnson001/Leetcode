dd20bd87f3f5b41cd2ca237d9e8699052d6b8138
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2024-11-09 16:13:58 -0500

    Time: 87 ms (5.02%) | Memory: 27.5 MB (72.03%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,26 +1,17 @@
-import sys
-class Solution(object):
-    def minSubArrayLen(self, target, nums):
-        """
-        :type target: int
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        ans=float("inf")
+        curr=0
         left=0
-        curr_sum=0
-        ans=float('inf')
         for right in range(len(nums)):
-            #creting the slidign window
-            if curr_sum<target:
-                curr_sum+=nums[right]
-                
-            while curr_sum>=target:
-                ans=min(ans,right-left+1)
-                #modifying the slidingwindow
-                curr_sum-=nums[left]
+            curr+=nums[right]
+            print(str(nums[right])+"-"+str(curr))
+            while(curr>=target):
+                ans=min(ans,(right-left)+1)
+                curr-=nums[left]
+                print(curr)
                 left+=1
+                
+        return ans if ans != float("inf") else 0
+
         
-        return 0 if ans==float('inf') else int(ans)
-
-
-
