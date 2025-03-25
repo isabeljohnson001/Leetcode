dc7e58200290d2b795aaff57f8e2637d13bfa5d1
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-03-25 16:36:01 -0400

    Time: 3 ms (70.96%) | Memory: 18.3 MB (79.40%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,23 +1,36 @@
-class Solution(object):
-    def setZeroes(self, matrix):
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        r=len(matrix)
-        c=len(matrix[0])
-        rows=set()
-        cols=set()
-        for i in range(r):
-            for j in range(c):
-                if matrix[i][j]==0:
-                    rows.add(i)
-                    cols.add(j)
 
-        for i in range(r):
-            for j in range(c):
-                if i in rows or j in cols:
-                    matrix[i][j]=0
+        rows=len(matrix)
+        cols=len(matrix[0])
+
+        rowZero=False
+
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c]==0:
+                    matrix[0][c]=0
+                    if r>0:
+                        matrix[r][0]=0
+                    else:
+                        rowZero=True
+        
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if matrix[0][c]==0 or matrix[r][0]==0:
+                    matrix[r][c]=0
+        
+        if matrix[0][0]==0:
+            for r in range(rows):
+                matrix[r][0]=0
+
+        if rowZero:
+            for c in range(cols):
+                matrix[0][c]=0
+        
         return matrix
 
         
