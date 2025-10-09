d85309422247a3ebe2f9e40cae88af62e680a84f
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:49:21 -0400

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,20 @@
+// Last updated: 10/8/2025, 9:49:21 PM
+class Solution {
+   public  String gcdOfStrings(String str1, String str2) {
+        for(int i=Math.min(str1.length(),str2.length());i>=1;i--){
+            if(isValid(str1,str2,i)){
+                return str1.substring(0,i);
+            }
+        }
+
+    return "";
+    }
+    
+    public  boolean isValid(String str1,String str2,int k){
+        if(str1.length()%k==0 && str2.length()%k==0){
+            String base=str1.substring(0,k);
+            return(str1.replace(base,"").isEmpty() && str2.replace(base,"").isEmpty());
+        }
+        else return false;
+    }
+}
