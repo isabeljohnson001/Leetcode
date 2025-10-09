196185f4a11cbdbf404ad420b874bd251312bbd9
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:48:46 -0400

    Create 1955-SeatReservationManager

---
+++ 1955-SeatReservationManager/1955-SeatReservationManager.py
@@ -0,0 +1,19 @@
+# Last updated: 10/8/2025, 9:48:46 PM
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.unres=[i for i in range(1, n+1)]
+
+    def reserve(self) -> int:
+        return heapq.heappop(self.unres)
+        
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.unres,seatNumber)
+        
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
