2f4c18798af0b126f02e3ae734c6a5eabda983d8
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:51:00 -0400

    Create 346-MovingAverageFromDataStream

---
+++ 346-MovingAverageFromDataStream/346-MovingAverageFromDataStream.java
@@ -0,0 +1,28 @@
+// Last updated: 10/8/2025, 9:51:00 PM
+class MovingAverage {
+        int size;
+        int val;
+
+        Queue<Integer> que=new LinkedList<>();
+        Deque window=new ArrayDeque<Integer>();
+        int sum=0;
+    public MovingAverage(int size) {
+        this.size=size;
+    }
+
+    public double next(int val) {
+        window.add(val);
+        int first=window.size()>size? (int)window.pollFirst():0;
+        sum+=val-first;
+        return 1.0* sum/ window.size();
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
+
+
+ 
