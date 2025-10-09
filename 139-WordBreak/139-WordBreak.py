2f89dc53fcc2f7a28a666823ee65b300331ecf48
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:52:13 -0400

    Create 139-WordBreak

---
+++ 139-WordBreak/139-WordBreak.py
@@ -0,0 +1,16 @@
+# Last updated: 10/8/2025, 9:52:13 PM
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        wordDict=set(wordDict)
+        n=len(s)
+        dp=[False]*(n+1)
+        dp[0] = True 
+        for i in range(1, n+1):
+            for j in range(i):
+                w=s[j:i]
+                if dp[j] and w in wordDict:
+                    dp[i]=True
+                    break
+        return dp[n]
+        
