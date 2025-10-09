f6e14150cf0c6933e528d7209938ff68f3f81b7c
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:50:00 -0400

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -0,0 +1,32 @@
+// Last updated: 10/8/2025, 9:50:00 PM
+class Solution {
+    public boolean checkValidString(String s) {
+    int minOpen = 0; // Minimum open parentheses needed
+    int maxOpen = 0; // Maximum open parentheses possible
+
+    for (char c : s.toCharArray()) {
+        if (c == '(') {
+            minOpen++;
+            maxOpen++;
+        } else if (c == ')') {
+            if (minOpen > 0) {
+                minOpen--; // A matching '(' can balance this ')'
+            }
+            maxOpen--;
+        } else { // c == '*'
+            if (minOpen > 0) {
+                minOpen--; // '*' acts as ')'
+            }
+            maxOpen++; // '*' acts as '('
+        }
+
+        if (maxOpen < 0) {
+            // More ')' than '(' + '*' can account for
+            return false;
+        }
+    }
+
+    // Valid if minOpen is 0 (all '(' can be matched)
+    return minOpen == 0?true:false; 
+    }
+}
