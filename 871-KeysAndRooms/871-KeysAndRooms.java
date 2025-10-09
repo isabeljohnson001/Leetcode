93015fb6fbb5d33ff9e00d01d0a4db0000e30ed1
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:49:48 -0400

    Create 871-KeysAndRooms

---
+++ 871-KeysAndRooms/871-KeysAndRooms.java
@@ -0,0 +1,25 @@
+// Last updated: 10/8/2025, 9:49:48 PM
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+     boolean[] seen=new boolean[rooms.size()];
+        Stack<Integer> s=new Stack();
+        seen[0]=true;
+        s.push(0);
+        while(!s.isEmpty()){
+            int node=s.pop();
+            for(int key:rooms.get(node)){
+             if(!seen[key]){
+                 seen[key]=true;
+                 s.push(key);
+                 
+             }   
+            }
+        }
+        for(boolean visit:seen){
+            if(!visit){return  false;}
+        }
+
+
+                return true;
+        }
+}
