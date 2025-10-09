800aa0497941734bb189da7c722828ddac976c2e
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:52:24 -0400

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+// Last updated: 10/8/2025, 9:52:24 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     public int minDepth(TreeNode root) {
+        return dfs(root);       
+    }
+
+    private int dfs(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        if(root.left==null){
+            return 1+dfs(root.right);
+        }else if(root.right==null){
+            return 1+dfs(root.left);
+        }
+        return 1+Math.min(dfs(root.left),dfs(root.right));
+    }
+      
+}
