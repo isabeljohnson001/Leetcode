4c44be0f06d21f73504977c8e8cc1693eb0c4b7a
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:47:55 -0400

    Create 2338-MinimumConsecutiveCardsToPickUp

---
+++ 2338-MinimumConsecutiveCardsToPickUp/2338-MinimumConsecutiveCardsToPickUp.java
@@ -0,0 +1,19 @@
+// Last updated: 10/8/2025, 9:47:55 PM
+class Solution {
+    public int minimumCardPickup(int[] cards) {
+        int ans=Integer.MAX_VALUE;
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int i=0;i<cards.length;i++){
+            int num=cards[i];
+            if(hs.containsKey(num)){
+                ans=Math.min(ans,i-hs.get(num)+1);
+            }
+            hs.put(num,i);
+        }
+
+        if(ans==Integer.MAX_VALUE){
+            return -1;
+        }
+        return ans;
+    }
+}
