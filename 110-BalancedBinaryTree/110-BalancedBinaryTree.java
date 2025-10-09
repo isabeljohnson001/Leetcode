a5b8774824031d6837cb9548a1a2d2bc7608fa32
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:52:27 -0400

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -0,0 +1,35 @@
+// Last updated: 10/8/2025, 9:52:27 PM
+
+    class TreeInfo{
+        int height;
+        boolean balanced;
+
+        TreeInfo(int height,boolean balanced){
+            this.height=height;
+            this.balanced=balanced;
+        }
+    }
+    class Solution{
+        public TreeInfo dfs(TreeNode root){
+            if(root==null){
+                return new TreeInfo(-1,true);
+            }
+            TreeInfo left=dfs(root.left);
+            if(!left.balanced){
+                return new TreeInfo(-1,false);
+            }
+            TreeInfo right=dfs(root.right);
+            if(!right.balanced){
+                return new TreeInfo(-1,false);
+            }
+            if(Math.abs(left.height- right.height)<2){
+                return new TreeInfo(Math.max(left.height,right.height)+1,true);
+            }
+
+            return new TreeInfo(-1,false);
+        }
+        
+        public boolean isBalanced(TreeNode root){
+            return dfs(root).balanced;
+        }
+    }
