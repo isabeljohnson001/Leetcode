a40c0484a93efd5a3b33ef43be471c1111c6dffa
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:52:05 -0400

    Create 186-ReverseWordsInAStringIi

---
+++ 186-ReverseWordsInAStringIi/186-ReverseWordsInAStringIi.cs
@@ -0,0 +1,30 @@
+// Last updated: 10/8/2025, 9:52:05 PM
+public class Solution {
+    public void ReverseWords(char[] s) {
+        int length=s.Length;
+        int start=0;
+        for(int i=0;i<length;i++){
+            if(s[i]==' ')
+            {
+                reverse(s,start,i-1);
+                start=i+1;
+            }
+        }
+        reverse(s,start,length-1);
+        reverse(s,0,length-1);
+
+}
+
+public void reverse(char[] input,int start,int end)
+{
+    while(start<end)
+    {
+    char temp=input[start];
+    input[start]=input[end];
+    start++;
+    input[end]=temp;
+    end--;
+    }
+   
+}
+}
