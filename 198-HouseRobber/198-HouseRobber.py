63c61124dc7fea4a6d02e94ddca04d2a6430a7f6
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:52:00 -0400

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.py
@@ -0,0 +1,18 @@
+# Last updated: 10/8/2025, 9:52:00 PM
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dp(i):
+            if i==0:
+                return nums[0]
+
+            if i==1:
+                return max(nums[0],nums[1])
+
+            return max(dp(i-1),dp(i-2)+nums[i])
+
+        return dp(len(nums)-1)
+
+
+
+        
