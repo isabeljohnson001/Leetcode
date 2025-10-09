c9042a8d1b90de80db32d65392d47b9c6a0bf70b
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:51:14 -0400

    Create 268-MissingNumber

---
+++ 268-MissingNumber/268-MissingNumber.py
@@ -0,0 +1,9 @@
+# Last updated: 10/8/2025, 9:51:14 PM
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        seen=set(nums)
+        for i in range(len(nums)+1):
+            if i not in seen:
+                return i
+        return 0
+        
