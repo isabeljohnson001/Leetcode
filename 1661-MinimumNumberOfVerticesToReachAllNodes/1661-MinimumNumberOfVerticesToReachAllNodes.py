12b083148074a37a7cf2f3697e828c7011b9113a
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:48:53 -0400

    Create 1661-MinimumNumberOfVerticesToReachAllNodes

---
+++ 1661-MinimumNumberOfVerticesToReachAllNodes/1661-MinimumNumberOfVerticesToReachAllNodes.py
@@ -0,0 +1,8 @@
+# Last updated: 10/8/2025, 9:48:53 PM
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        indegree=[0]*n
+        for _,y in edges:
+            indegree[y]+=1
+        
+        return [node for node in range(n) if indegree[node]==0]
