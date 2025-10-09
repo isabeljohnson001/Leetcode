22c60b680047c4fa69ab9f3587b0e4b83f4d4f8f
Isabel Johnson <120644893+isabeljohnson001@users.noreply.github.com>
2025-10-08 21:49:54 -0400

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.py
@@ -0,0 +1,20 @@
+# Last updated: 10/8/2025, 9:49:54 PM
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+
+        def dp(i):
+
+            if i<=1:
+                return 0
+
+            if i in memo:
+                return memo[i]
+
+
+            memo[i]=min(dp(i-1)+cost[i-1],dp(i-2)+cost[i-2])
+            return memo[i]
+
+        memo={}
+        return dp(len(cost))
+        
